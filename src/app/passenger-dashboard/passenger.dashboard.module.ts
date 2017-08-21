ff4c4e42450dc552a9fb6f5288173fc4834c8f50
Eric Xiao <e.xiao@sap.com>
2017-08-21 10:30:09 -0400

    child route

--- src/app/passenger-dashboard/passenger.dashboard.module.ts
+++ src/app/passenger-dashboard/passenger.dashboard.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms"
+import {Routes, RouterModule} from "@angular/router"
 
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component'
 import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component'
@@ -11,6 +12,23 @@ import {PassengerDetailComponent} from './components/passenger-detail/passenger-
 import {PassengerFormComponent} from './components/passenger-form/passenger-form.component'
 
 import {PassengerDashboardService} from './passenger-dashboard.service'
+
+const routes: Routes = [
+    {
+        path : 'passengers',
+        children:[
+            {
+                path:'',
+                component:PassengerDashboardComponent
+            },
+            {
+                path:':id',
+                component:PassengerViewerComponent
+            }
+        ],
+
+    }
+]
 
 @NgModule({
     declarations: [
@@ -24,11 +42,12 @@ import {PassengerDashboardService} from './passenger-dashboard.service'
     imports: [
         CommonModule,
         HttpModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forChild(routes)
     ],
-    exports:[
-        PassengerViewerComponent
-    ],
+    // exports:[
+    //     PassengerViewerComponent
+    // ],
     providers:[
         PassengerDashboardService
     ]
